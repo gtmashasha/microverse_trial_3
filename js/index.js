3de60ef1f375f75571dc7073fec454d1a5aeda01
nparfait101 <parfait@eagi.rw>
2020-04-10 15:59:37 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,28 +1,21 @@
-// !! IMPORTANT README:
+// Responsive Nav
+$(function() {
+	menu = $('nav ul');
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+  $('#openup').on('click', function(e) {
+    e.preventDefault(); menu.slideToggle();
+  });
 
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
+  $(window).resize(function(){
+    var w = $(this).width(); if(w > 480 && menu.is(':hidden')) {
+      menu.removeAttr('style');
+    }
+  });
 
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
-"use strict";
+  $('nav li').on('click', function(e) {
+    var w = $(window).width(); if(w < 480 ) {
+      menu.slideToggle();
+    }
+  });
+  $('.open-menu').height($(window).height());
+});
